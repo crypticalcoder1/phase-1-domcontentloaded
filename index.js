396d38a114bba0cab9b3051007ae0aed9f186ac4
crypticalcoder1 <kirambawycliff@gmail.com>
2024-07-09 11:59:38 +0300

    my file

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// Add event listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Target the paragraph element with id="text" and replace its text content
+    const textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+  });
+  
